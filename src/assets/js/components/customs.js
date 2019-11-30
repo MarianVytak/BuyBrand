add9efec5ac35b593dc1a16011f872c8f77e054d
MarianVytak <marianvytak@gmail.com>
2019-11-30 12:50:40 +0200

    Added Popovers

--- src/assets/js/components/customs.js
+++ src/assets/js/components/customs.js
@@ -1,7 +1,17 @@
-'use strict';
+$(function () {
 
-$('.dropdown-item').on('click', function () {
-    let thisText = $(this).text();
-    let thisDropdown = $(this).attr('data-target');
-    $(thisDropdown).text(thisText);
+    'use strict';
+
+    // Заміна тексту для dropdown
+    $('.dropdown-item').on('click', function () {
+        let thisText = $(this).text();
+        let thisDropdown = $(this).attr('data-target');
+        $(thisDropdown).text(thisText);
+    });
+
+    // Popover
+    $('[data-toggle="popover"]').popover({
+        container: 'body'
+    });
+
 });
