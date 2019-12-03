23478105bf0c85ef0272836747f9027c2d8a7c8f
MarianVytak <marianvytak@gmail.com>
2019-12-04 00:55:11 +0200

    Добавив слайдер для підгруп.

--- src/templates/main/chart-table/chart-table.js
+++ src/templates/main/chart-table/chart-table.js
@@ -2,6 +2,15 @@ $(function () {
 
     'use strict';
 
+    $('.chart-table__subgroup').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 6,
+        variableWidth: true,
+        initialSlide: 0,
+    });
+
     $('.chart-table__block_list_all').on('click', function (e) {
         e.preventDefault();
         let thisList = $(this).attr('href');
@@ -11,15 +20,15 @@ $(function () {
 
     $(window).on("load",function(){
 
-        $(".chart-table__subgroup").mCustomScrollbar({
-            theme: 'dark',
-            horizontalScroll: true,
-            autoHideScrollbar: false,
-            //scrollButtons: true,
-            mouseWheel: {
-                enable: true,
-            }
-        });
+        // $(".chart-table__subgroup").mCustomScrollbar({
+        //     theme: 'dark',
+        //     horizontalScroll: true,
+        //     autoHideScrollbar: false,
+        //     //scrollButtons: true,
+        //     mouseWheel: {
+        //         enable: true,
+        //     }
+        // });
 
         $(".chart-table__block_list").mCustomScrollbar();
 
