eafe2da9de759cd12e49952dfa8f330bc5e37f57
MarianVytak <marianvytak@gmail.com>
2019-12-08 00:56:53 +0200

    Переробив зміну форм.

--- src/assets/js/components/customs.js
+++ src/assets/js/components/customs.js
@@ -18,6 +18,36 @@ $(function () {
         $('.sidebar__toggle_link_chart-table').addClass('active');
     }
 
+    // Переключення форм в модальному вікні
+    const placingForm1Btn = $('.placingForm-1__btn');
+    const placingForm2Btn = $('.placingForm-2__btn');
+    const placingForm1 = $('.placingForm-1');
+    const placingForm2 = $('.placingForm-2');
+    const placingForm3 = $('.placingForm-3');
+    const placingStep1 = $('.step-back-1');
+    const placingStep2 = $('.step-back-2');
+    placingForm1Btn.on('click', function() {
+        placingForm1.fadeOut();
+        placingForm2.fadeIn();
+    });
+    placingForm2Btn.on('click', function() {
+        placingForm2.fadeOut();
+        placingForm3.fadeIn();
+    });
+    $('.close').on('click', function() {
+        placingForm1.fadeIn();
+        placingForm2.fadeOut();
+        placingForm3.fadeOut();
+    });
+    placingStep1.on('click', function() {
+        placingForm2.fadeOut();
+        placingForm1.fadeIn();
+    });
+    placingStep2.on('click', function() {
+        placingForm3.fadeOut();
+        placingForm2.fadeIn();
+    });
+
     // Popover
     $('[data-toggle="popover"]').popover({
         trigger: 'hover'
